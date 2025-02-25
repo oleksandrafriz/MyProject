c3fca7033bb1e0b171b980f082f205cb4e4e1892
oleksandrafriz <ipz221_foi@student.ztu.edu.ua>
2025-02-25 19:56:49 +0200

    added cookie popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Write from "./components/Write";
-import Read from "./components/Read";
-import UpdateRead from "./components/UpdateRead";
-import UpdateWrite from "./components/UpdateWrite";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import CartoonDetails from "./components/CartoonDetails";
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
     <>
+      <CookieConsent
+        location="bottom"
+        buttonText="Зрозуміло"
+        cookieName="myCookieConsent"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{
+          color: "#4e503b",
+          fontSize: "15px",
+          fontWeight: "bold",
+          borderRadius: "2px",
+        }}
+        expires={1}
+      >
+        Цей сайт використовує cookies для покращення вашого досвіду. Продовжуючи
+        використовувати сайт, ви погоджуєтесь з нашою{" "}
+        <a href="/privacy-policy" style={{ color: "#fff" }}>
+          Політикою конфіденційності
+        </a>
+        .
+      </CookieConsent>
       <Router>
         <Navbar />
         <Routes>
